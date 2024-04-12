664aa7b90d9ec4b6bbb561e3c2879ffbbec0c668
smnraphael <simonraphael@hotmail.fr>
2024-04-12 18:15:20 +0200

    first commit

---
+++ backend/models/User.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
