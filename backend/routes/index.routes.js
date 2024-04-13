3ed77edfbbdb754276aaeff05a04692513fb83b4
smnraphael <simonraphael@hotmail.fr>
2024-04-13 13:25:59 +0200

    joboffers routes done

--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -7,5 +7,6 @@ router.get("/", (req, res, next) => {
 router.use("/auth", require("./auth.routes.js"));
 router.use("/users", require("./users.routes.js"));
 router.use("/companies", require("./companies.routes.js"));
+router.use("/joboffers", require("./joboffers.routes.js"));
 
 module.exports = router;
