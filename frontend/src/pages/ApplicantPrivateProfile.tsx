8d552438595ffe2d3eecc0b2ce144d9f4449f92a
smnraphael <simonraphael@hotmail.fr>
2024-04-20 15:49:46 +0200

    applicant profile done

--- frontend/src/pages/ApplicantPrivateProfile.tsx
+++ frontend/src/pages/ApplicantPrivateProfile.tsx
@@ -4,46 +4,51 @@ import useAuth from "../context/useAuth";
 const ApplicantPrivateProfile = () => {
   const { user } = useAuth();
   return (
-    <div className="flex flex-col items-center sm:items-start gap-5 justify-between p-5 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-600">
-      {user?.__t === "Applicant" && (
-        <>
-          <div className="flex flex-col sm:flex-row gap-5">
-            <img
-              src={user.image}
-              alt={user.firstName}
-              className="h-36 rounded-lg"
-            />
-            <div className="flex flex-col gap-2">
-              <p className="font-semibold dark:text-white">{user.firstName}</p>
-              <p className="font-semibold dark:text-white">{user.lastName}</p>
-              <p className="text-sm text-gray-600 dark:text-gray-300">
-                {user.email}
-              </p>
-            </div>
-          </div>
-          <Link
-            to="/applicant/private-profile/applications"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
-          >
-            See all my applications
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
+    <div className="flex flex-col gap-5">
+      <h1 className="text-xl font-bold">My Profile</h1>
+      <div className="flex flex-col items-center sm:items-start gap-5 justify-between p-5 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-600">
+        {user?.__t === "Applicant" && (
+          <>
+            <div className="flex flex-col sm:flex-row gap-5">
+              <img
+                src={user.image}
+                alt={user.firstName}
+                className="h-36 rounded-lg"
               />
-            </svg>
-          </Link>
-        </>
-      )}
+              <div className="flex flex-col gap-2">
+                <p className="font-semibold dark:text-white">
+                  {user.firstName}
+                </p>
+                <p className="font-semibold dark:text-white">{user.lastName}</p>
+                <p className="text-sm text-gray-600 dark:text-gray-300">
+                  {user.email}
+                </p>
+              </div>
+            </div>
+            <Link
+              to="/applicant/private-profile/applications"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+            >
+              See all my applications
+              <svg
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+          </>
+        )}
+      </div>
     </div>
   );
 };
