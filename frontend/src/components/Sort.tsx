d26898f535d5efd581c7717782f831b693e88428
smnraphael <simonraphael@hotmail.fr>
2024-04-21 12:35:17 +0200

    sort fixed with typescript

--- frontend/src/components/Sort.tsx
+++ frontend/src/components/Sort.tsx
@@ -1,31 +1,47 @@
-// import useJob from "../context/useJob";
+import useJob from "../context/useJob";
+
+type Job = {
+  _id: string;
+  company: {
+    _id: string;
+    image: string;
+    name: string;
+  };
+  position: string;
+  positionOverview: string;
+  employmentType: string;
+  workLevel: string;
+  remote: string;
+  salary: number;
+  createdAt: Date;
+};
 
 const Sort = () => {
-  // const { jobs, setJobs } = useJob();
+  const { jobs, setJobs } = useJob();
 
-  // const handleSort = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  //   const sortOption = e.currentTarget.value;
+  const handleSort = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const sortOption = e.currentTarget.value;
 
-  //   if (jobs) {
-  //     setJobs((prevJobs) => {
-  //       let sortedJobs = [...prevJobs];
+    if (jobs) {
+      setJobs((prevJobs) => {
+        let sortedJobs = [...(prevJobs as Job[])];
 
-  //       if (sortOption === "newest") {
-  //         sortedJobs.sort(
-  //           (a, b) =>
-  //             new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-  //         );
-  //       } else if (sortOption === "oldest") {
-  //         sortedJobs.sort(
-  //           (a, b) =>
-  //             new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-  //         );
-  //       }
+        if (sortOption === "newest") {
+          sortedJobs.sort(
+            (a, b) =>
+              new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          );
+        } else if (sortOption === "oldest") {
+          sortedJobs.sort(
+            (a, b) =>
+              new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+          );
+        }
 
-  //       return sortedJobs;
-  //     });
-  //   }
-  // };
+        return sortedJobs;
+      });
+    }
+  };
 
   return (
     <form className="hidden sm:flex items-center">
@@ -38,7 +54,7 @@ const Sort = () => {
       <select
         id="sortSelect"
         name="sortSelect"
-        // onChange={handleSort}
+        onChange={handleSort}
         className="bg-white border border-gray-200 text-gray-900 text-sm rounded-lg py-1 px-2 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300"
       >
         <option value="newest">Newest</option>
