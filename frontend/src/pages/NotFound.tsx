97c0f6b3a3171a52c5965fc7d62036ede936928e
smnraphael <simonraphael@hotmail.fr>
2024-04-24 10:28:48 +0200

    not found page added

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex justify-center">
+      <p className="text-lg font-bold dark:text-white">Not found</p>
+    </div>
+  );
+};
+
+export default NotFound;
